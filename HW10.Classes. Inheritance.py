096ac4d9338708792e1cd8dcd0e6dd80f79449b3
StupakVitalii <145101704+StupakVitalii@users.noreply.github.com>
2023-11-25 15:53:28 +0200

    Add files via upload
    StupakVitalii-Home_work_10.Classes. Inheritance

---
+++ HW10.Classes. Inheritance.py
@@ -0,0 +1,44 @@
+#Ex1.
+class BaseClass:
+    pass
+#Ex2.
+class DerivedClass(BaseClass):
+    def __init__(self, base_param, derived_param):
+        super().__init__()
+        self.base_param = base_param
+        self.derived_param = derived_param
+
+    def display_params(self):
+        print(f'Base parameter: {self.base_param}, Derived Parameter: {self.derived_param}')
+
+derived_instance = DerivedClass('Base Value', 'Derived Value')
+derived_instance.display_params()
+
+#Ex3.
+
+class ClassWithNoReturn:
+    def return_fields(self):
+        pass
+
+#Ex4.
+
+class ClassWithTwoParams(ClassWithNoReturn):
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+
+    def return_fields(self):
+        print(f'Parameters : {self.param1}, {self.param2}')
+
+#Ex5.
+
+class ClassWithThreeParams (ClassWithNoReturn):
+    def __init__(self, param1, param2, param3):
+        self.param1 = param1
+        self.param2 = param2
+        self.param3 = param3
+    def return_fields(self):
+        print(f'Parameters : {self.param1}, {self.param2}, {self.param3}')
+
+instance_three_params = ClassWithThreeParams('Value1', 'Value2', 'Value3')
+instance_three_params.return_fields()
